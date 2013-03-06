1521861caa3ed9737dee3d74635b9c1397acc386
David Gay <oddshocks@gmail.com>
2013-03-06 14:07:20 -0500

    makes skeleton directories

--- pythong/project.py
+++ pythong/project.py
@@ -12,6 +12,17 @@ class Project(object):
 
     def __init__(self, name):
         self.name = name
-        bash_command = "mkdir " + join(_here, name)
-        print "running command: " + bash_command
+        self.project_dir = join(_here, name)
+        self.bin_dir = join(self.project_dir, "bin")
+        self.docs_dir = join(self.project_dir, "docs")
+        self.source_dir = join(self.project_dir, name)
+        self.tests_dir = join(self.project_dir, "tests")
+
+        # Create structure
+        print "Creating structure for new Python project " + \
+                self.name
+        # Create directories
+        bash_command = "mkdir {0} {1} {2} {3} {4}".format(
+                self.project_dir, self.bin_dir, self.docs_dir,
+                self.source_dir, self.tests_dir)
         subprocess.Popen(bash_command, shell=True)
