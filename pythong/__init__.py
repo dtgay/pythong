26adcd82e4ff43993252b9f03f2b5811082155df
David Gay <oddshocks@gmail.com>
2013-03-08 12:28:58 -0500

    add --version command

--- pythong/__init__.py
+++ pythong/__init__.py
@@ -8,6 +8,9 @@ from pythong.project import prompt_new_project
 def create_project():
     p = argparse.ArgumentParser()
     p.add_argument('name', help='name of project to be created')
+    # TODO: have version be pulled from setup.py variable?
+    p.add_argument('--version', action='version',
+                    version='you are using version 0.0.1 of the pythong')
     p.add_argument('--snap', action='store_true',
                     help='quickly create a project skeleton without \
                           any prompting')
