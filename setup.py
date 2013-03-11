227067f39c20dcfe1d7f9a1ba71cc4abbea04970
Ralph Bean <rbean@redhat.com>
2013-03-11 14:59:51 -0400

    Allow use of "python setup.py test"

--- setup.py
+++ setup.py
@@ -27,9 +27,11 @@ setup(name='pythong',
       url='http://github.com/oddshocks/pythong',
       license='GPLv3+',
       packages=find_packages(exclude=['ez_setup', 'tests']),
+      test_suite='nose.collector',
+      tests_require=['nose', 'mock'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=['nose', 'mock', 'six', 'jinja2'],
+      install_requires=['six', 'jinja2'],
       entry_points="""
       [console_scripts]
       pythong = pythong:create_project
