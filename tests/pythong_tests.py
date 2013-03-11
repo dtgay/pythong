36581fe387e0239ec26bae7d296a90a4cb50a8c8
Ralph Bean <rbean@redhat.com>
2013-03-11 15:00:16 -0400

    Use the stdlib unittest module to allow "python tests/pythong_tests.py"

--- tests/pythong_tests.py
+++ tests/pythong_tests.py
@@ -1,6 +1,7 @@
 #!/bin/env python
 # -*- coding: utf8 -*-
 from nose.tools import *
+import unittest
 import pythong
 from pythong.util import Directory
 
@@ -10,22 +11,24 @@ from nose.tools import eq_
 import tempfile
 
 
-def setup():
-    print "SETUP"
+class TestThong(unittest.TestCase):
+    def setUp(self):
+        print "SETUP"
 
+    def tearDown(self):
+        print "TEAR DOWN"
 
-def teardown():
-    print "TEAR DOWN"
+    @patch("sys.argv", new_callable=lambda: ["pythong", "--snap", "test_proj"])
+    def test_snap_project(self, mock_argv):
+        with Directory(tempfile.mkdtemp()):
+            # below, I comment out the proper assertion line and assert
+            # True to show that pythong.create_project() is what is causing
+            # tests to fail with:
+            # TypeError: sequence item 0:
+            #           expected string or Unicode, exceptions.SystemExit found
+            pythong.create_project()
+            #assert(os.path.isdir("tests"))
+            assert(True)
 
-
-@patch("sys.argv", new_callable=lambda: ["pythong", "--snap", "test_proj"])
-def test_snap_project(mock_argv):
-    with Directory(tempfile.mkdtemp()):
-        # below, I comment out the proper assertion line and assert
-        # True to show that pythong.create_project() is what is causing
-        # tests to fail with:
-        # TypeError: sequence item 0:
-        #           expected string or Unicode, exceptions.SystemExit found
-        pythong.create_project()
-        #assert(os.path.isdir("tests"))
-        assert(True)
+if __name__ == '__main__':
+    unittest.main()
