11e4d3f69b10fd451018ba1f3fe433a9c7d5ad31
David Gay <oddshocks@gmail.com>
2013-03-12 18:03:39 -0400

    commands allowing short and long arguments now work

--- pythong/__init__.py
+++ pythong/__init__.py
@@ -13,10 +13,10 @@ def parse_command():
     # TODO: have version be pulled from setup.py variable?
     p.add_argument('--version', action='version',
                    version='you are using version 0.0.1 of the pythong')
-    p.add_argument('-s, --snap', action='store_true',
+    p.add_argument('-s', '--snap', action='store_true',
                    help='quickly create a project skeleton without \
                           any prompting')
-    p.add_argument('-w, --wash', action='store_true',
+    p.add_argument('-w', '--wash', action='store_true',
                    help='clean your pythong of messy build/dist/egg files')
     args = p.parse_args()
     if args.wash:
--- pythong/command.py
+++ pythong/command.py
@@ -1,7 +1,12 @@
 #!/bin/env python
 # -*- coding: utf8 -*-
 import os
+import re
 
 
 def wash():
+    """Remove all build/dist/egg-related files"""
     cwd = os.getcwd()
+    for f in os.listdir(cwd):
+        if re.search('build|dist|*egg-info', f):
+            os.remove(os.path.join(cwd, f))
